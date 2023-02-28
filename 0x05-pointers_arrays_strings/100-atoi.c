59695e7b12e877b40b535874ce51f81096222373
olushola54 <olushola54@gmail.com>
2023-02-27 20:50:45 -0800

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@
  * Return: the int converted from the string
 */
 int _atoi(char *s)
-{{
+{
 	int i, d, n, len, f, digit;
 
 	i = 0;
@@ -26,6 +26,7 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 	{
 			digit = s[i] - '0';
+
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
